c8eec8979d20ac82c3e826eaf2803122e3936305
Kaique-Dirceu <dircekaique@gmail.com>
2020-06-07 22:22:02 -0300

    Finish

---
+++ public/scripts/create-point.js
@@ -0,0 +1,74 @@
+function populateUFs() {
+    const ufSelect = document.querySelector("select[name=uf]")
+
+    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+    .then( res => res.json() )
+    .then( states => {
+        for( const state of states ) {
+            ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
+        }
+    } )
+}
+
+populateUFs()
+
+function getCities(event) {
+    const citySelect = document.querySelector("select[name=city]")
+    const stateInput = document.querySelector("input[name=state]")
+    const ufValue = event.target.value
+    const indexOfSelectState = event.target.selectedIndex
+    stateInput.value = event.target.options[indexOfSelectState].text 
+    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
+
+    citySelect.innerHTML = "<option value>Selecione a cidade</option>"
+    citySelect.disabled = true
+
+    fetch(url)
+    .then( res => res.json() )
+    .then( cities => {
+        for( const city of cities ) {
+            citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
+        }
+        citySelect.disabled = false
+    } )
+}
+
+document
+    .querySelector("select[name=uf]")
+    .addEventListener("change", getCities)
+
+//items de coleta
+
+const itemsToCollect = document.querySelectorAll(".items-grid li")
+
+for (const item of itemsToCollect) {
+    item.addEventListener('click', headleSelectedItem)
+}
+
+const collectedItems = document.querySelector("input[name=items]")
+
+let selectedItems = []
+
+function headleSelectedItem(event) {
+    const itemLi = event.target
+    itemLi.classList.toggle("selected")
+    console.log(event.target.dataset.id)
+
+    const itemId = itemLi.dataset.id
+    const alreadySelected = selectedItems.findIndex( item =>{
+        const itemFound = item == itemId
+        return itemFound 
+    })
+
+    if(alreadySelected >= 0){
+        const filteredItems = selectedItems.filter(item => {
+            const itemIsDefferent = item != itemId
+            return itemIsDefferent
+        })
+        selectedItems = filteredItems 
+    }else{
+        selectedItems.push(itemId)
+    }
+
+    collectedItems.value = selectedItems
+}
